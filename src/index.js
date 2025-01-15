be3da1b6d9f82358e4d3c755a2dca1a905a9b0ca
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-01-15 18:54:30 +0200

    Add 404 page

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.render('about');
-})
+});
+
+app.all('*', (req, res) => {
+    res.render('404');
+});
 
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
